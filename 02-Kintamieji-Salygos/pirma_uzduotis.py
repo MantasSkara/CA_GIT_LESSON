e510fe55632e1ef481cd644f266741ef37a012dd
AmnesIIa91 <gediminas.gedukas@gmail.com>
2022-05-26 20:57:32 +0300

    pakeistas

--- 02-Kintamieji-Salygos/pirma_uzduotis.py
+++ 02-Kintamieji-Salygos/pirma_uzduotis.py
@@ -1,5 +1,5 @@
-a = int(input("Įveskite skaičių yyyy"))
-b = float(input("Įveskite skaičiųy yyy "))
+a = int(input("Įveskite skaičių "))
+b = float(input("Įveskite skaičių "))
 if b > a:
   print("a mažesnis už b")
   print("dalyba: " + str(b/a))
@@ -10,9 +10,3 @@ else:
   print("a didesnis už b")
   print("dalyba: " + str(a/b))
 
-  # if a == b:
-#   print("a lygu b")
-#   print("Sudėtis: " + str(a+b))
-# if a > b:
-#   print("a didesnis už b")
-#   print("dalyba: " + str(a/b))
